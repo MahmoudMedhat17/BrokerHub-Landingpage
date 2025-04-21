00c83c0c6fedcfa9c9c2fcfa5da801d450a0f6b2
Mahmoud Medhat <M7moooud.17@gmail.com>
2025-04-21 19:25:51 +0200

    Fixed Sheet bug.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,16 @@ import {
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import {Button} from "@/components/ui/button";
 import { Menu } from 'lucide-react';
 import { navLinks } from "../lib";
 import { ScrollToSec } from "@/lib/ScrollToSec";
+import {Button} from "@/components/ui/button";
 
 
 const Navbar = () => {
+
+  
+
   return (
     <header id="navbar" className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between items-center">
@@ -31,25 +34,29 @@ const Navbar = () => {
           <p className="hidden md:flex items-center gap-2">Arabic <Globe className="w-4 h-4"/></p>
         </div>
         <div className="flex items-center justify-end md:hidden">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button 
-              variant="default"
-              className="rounded-sm transition bg-transparent cursor-pointer">
-                <Menu className="!w-7 !h-7"/>
-              </Button>
-            </SheetTrigger>
-            <SheetContent>
-              {
-                navLinks.map((titles,index)=>(
-                  <div onClick={()=>ScrollToSec(titles.id)} key={index}>
-                    <p className="cursor-pointer text-xl font-semibold text-[#1B263B]">{titles.title}</p>
-                  </div>
-                ))
-              }
-              <p className="flex md:hidden items-center gap-2 text-[#1B263B] text-xl font-semibold">Arabic <Globe/></p>
-            </SheetContent>
-          </Sheet>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button 
+            variant="default"
+            className="rounded-sm transition bg-transparent cursor-pointer">
+              <Menu className="!w-7 !h-7"/>
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+          // These functions to disable autofocus of radix used in sheet that shifts to the top of the landing page by itself.
+            onOpenAutoFocus={(e)=> e.preventDefault()}
+            onCloseAutoFocus={(e)=> e.preventDefault()}
+          >
+            {
+              navLinks.map((titles,index)=>(
+                <div onClick={()=>ScrollToSec(titles.id)} key={index}>
+                  <p className="cursor-pointer text-xl font-semibold text-[#1B263B]">{titles.title}</p>
+                </div>
+              ))
+            }
+            <p className="flex md:hidden items-center gap-2 text-[#1B263B] text-xl font-semibold">Arabic <Globe/></p>
+          </SheetContent>
+        </Sheet>
         </div>
       </div>
     </header>
@@ -57,3 +64,27 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+
+<Sheet>
+  <SheetTrigger asChild>
+    <Button 
+    variant="default"
+    className="rounded-sm transition bg-transparent cursor-pointer">
+      <Menu className="!w-7 !h-7"/>
+    </Button>
+  </SheetTrigger>
+  <SheetContent>
+    {
+      navLinks.map((titles,index)=>(
+        <div onClick={()=>ScrollToSec(titles.id)} key={index}>
+          <p className="cursor-pointer text-xl font-semibold text-[#1B263B]">{titles.title}</p>
+        </div>
+      ))
+    }
+    <p className="flex md:hidden items-center gap-2 text-[#1B263B] text-xl font-semibold">Arabic <Globe/></p>
+  </SheetContent>
+</Sheet>
